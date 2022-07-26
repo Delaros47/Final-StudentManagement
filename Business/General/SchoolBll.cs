0fe46b9cf20ffce290ab18f64d022ee449089528
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-07-26 21:57:36 +0300

    ...

---
+++ Business/General/SchoolBll.cs
@@ -0,0 +1,32 @@
+﻿using Business.Base;
+using Data.Contexts;
+using Model.Entities;
+using Model.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Business.General
+{
+    public class SchoolBll : BaseBll<School, StudentManagementContext>
+    {
+        protected SchoolBll()
+        {
+
+        }
+
+        protected SchoolBll(Control ctrl) : base(ctrl)
+        {
+
+        }
+
+        public BaseEntity Single(Expression<Func<School,bool>> filter)
+        {
+            return BaseSingle(filter,);
+        }
+    }
+}
