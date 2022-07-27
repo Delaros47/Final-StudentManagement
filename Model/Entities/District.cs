bddf8cfffd63557000f1828d7567e5dc0662e5db
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-07-27 14:02:05 +0300

    ...

--- Model/Entities/District.cs
+++ Model/Entities/District.cs
@@ -1,16 +1,28 @@
 ﻿using Model.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model.Entities
 {
+    #region Comment
+    /*
+     * Here the reason IsUnique that we have put false that for example Mardin has Derik and Kızıltepe it will be District-00001 and District-00002 and at the same time Antalya also has Alanya and Manavgat it will be District-00001 and District-00002 with different will be their CityId because when we open Antalya Districts and it will be listed its Districts so better to start from District-00001 and District-00002 if we put IsUnique =true then it will start from 1 until different number District Private Code will get but we don't want something like that
+     */
+    #endregion
+
     public class District:BaseEntityState
     {
+        [Index("IX_PrivateCode",IsUnique =false)]
+        public override string PrivateCode { get; set; }
+        [Required,StringLength(50)]
         public string DistrictName { get; set; }
         public int CityId { get; set; }
+        [StringLength(500)]
         public string Description { get; set; }
 
         public City City { get; set; }
--- Model/Entities/School.cs
+++ Model/Entities/School.cs
@@ -1,17 +1,30 @@
 ﻿using Model.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model.Entities
 {
-    public class School:BaseEntityState
+    #region Comment
+    /*
+     * [Index("IX_PrivateCode",IsUnique =true)] Here since we always make Order By by Private Code so we will use it often if we make it as Index that it will work faster and I named it as IX_PrivateCode  and also IsUnique =true it will be unique we cannot write the same Private Code again
+     */
+    #endregion
+
+    public class School : BaseEntityState
     {
+
+        [Index("IX_PrivateCode",IsUnique =true)]
+        public override string PrivateCode { get; set; }
+        [Required, StringLength(75)]
         public string SchoolName { get; set; }
         public int CityId { get; set; }
         public int DistrictId { get; set; }
+        [StringLength(500)]
         public string Description { get; set; }
 
 
